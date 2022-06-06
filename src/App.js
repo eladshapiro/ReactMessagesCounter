35aa827621b0adcb07045bd38b4b0a4a4e752779
Elad shapiro <eladshapiro@gmail.com>
2022-06-06 13:03:58 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component
+{
+
+  state = {
+    massages: [],
+    massage: ""
+  }
+
+
+
+  firstInput = (e) => {
+    const massage = e.target.value;
+    this.setState({
+      firstName: massage
+    })
+  }
+
+
+  addMassage = () => {
+
+    let newMassage;
+
+    newMassage = {
+      massage: this.state.massage
+    }
+
+    this.state.massages.push(newMassage)
+    this.setState({
+      massage: "",
+    })
+  }
+
+
+  clearTable = () => {
+    this.setState({
+      massages: []
+    })
+  }
+
+  deleteOne = (i) => {
+    this.state.massages.splice(i, 1)
+    this.setState({})
+  }
+
+  render() {
+    return(
+        <div>
+          <input placeholder = "Enter A Massage:" value={this.state.massage} onChange={this.firstInput}/>
+          <button onClick={this.addMassage} disabled={this.state.massage === "" }>Add massage</button>
+          <table>
+            {
+                this.state.massages.length > 0 &&
+                this.state.massages.map((item, i) => {
+                  return (
+                      <tr key = {i} onClick={this.deleteOne.bind(this, i)}>
+                        <td>{item.massage}</td>
+                      </tr>
+                  )
+                })
+            }
+          </table>
+          <h6>{this.state.massages.length === 0 ? "No massages" : "number of messages: " + this.massages.length}</h6>
+
+          <button disabled={this.state.massages.length === 0} onClick={this.clearTable}>Clear</button>
+
+
+        </div>
+
+
+    )
+  }
 }
-
 export default App;
